155f9da2cd315ae0b13a9947ca0a3085c556e175
slanger <s.mark.langer@outlook.com>
2014-03-10 15:02:26 -0400

    PeerProcess started

--- src/PeerProcess.java
+++ src/PeerProcess.java
@@ -1,10 +1,22 @@
-public class PeerProcess{
+public class PeerProcess
+{
 
+	public static void main(String[] args)
+	{
+		if (args.length != 1)
+		{
+			System.out.println("Usage: java peerProcess <peer_ID>");
+		}
 
-	public static void main(String[] args){
+		int peerID = Integer.parseInt(args[0]);
 
-
-
-
+		PeerProcess peerProcess = new PeerProcess();
+		peerProcess.run(peerID); // so every method doesn't have to be static
 	}
+
+	public void run(int peerID)
+	{
+		
+	}
+
 }
