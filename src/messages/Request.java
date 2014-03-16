b77b8f9dd2f56cca298b28adc7021fbd978b1e6e
chy <sylvantier@gmail.com>
2014-03-16 15:56:45 -0400

    Added a variable to Request

--- src/messages/Request.java
+++ src/messages/Request.java
@@ -1,6 +1,9 @@
 package messages;
 
 public class Request extends Message {
+	
+	public int pieceIndex; //index of the requested piece 
+	
 	public Request(int senderID, int receiverID){
 		super(senderID, receiverID);
 	}
