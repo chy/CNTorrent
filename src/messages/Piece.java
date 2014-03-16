f590d2630bae5eaad09a12fd0b3568f08fb4cb22
chy <sylvantier@gmail.com>
2014-03-16 18:27:44 -0400

    Implemented stuff in Piece

--- src/messages/Piece.java
+++ src/messages/Piece.java
@@ -3,15 +3,16 @@ package messages;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
 import java.util.Scanner;
 
+import peers.NeighborPeer;
 import peers.Peer;
-
+//UNFINISHED, but compiles. 
 public class Piece extends Message{
 	private int pieceIndex; //id of the piece being sent
 	public byte [] piece; //byte array of the contents of the piece to be sent
-	
-	
 	
 	public Piece (int senderID, int receiverID, int pieceIndex){
 		super(senderID, receiverID);
@@ -32,13 +33,53 @@ public class Piece extends Message{
 		 * 			 request another random piece that you are interested in from it	 * 
 		 * 
 		 */
+		//PART 1
 		
+		writePieceToFile(piece);
 		
+		NeighborPeer sendingPeer = Peer.peers.get(this.senderID); 
+		boolean [] myBitfield = Peer.bitfield;
+		myBitfield[pieceIndex] = true; 
+		
+		for(NeighborPeer peer : Peer.peers.values()){ //for each peer
+			//check if you are interested in them
+			for(int i = 0; i < peer.bitfield.length; i++){
+				if(peer.bitfield[i] && !myBitfield[i]){
+					peer.amInterested = true;
+					break; 
+				}
+			}
+			//send them a have message
+			Peer.sendMessage(new Have(receiverID, senderID, pieceIndex)); 
+		}
+		
+		//PART 2
+		if(!sendingPeer.peerChoking && sendingPeer.amInterested){
+			
+			Random random = new Random();
+			int pieceDex = random.nextInt(myBitfield.length);
+			while(!(sendingPeer.bitfield[pieceDex] && !myBitfield[pieceDex])){
+				pieceDex = random.nextInt(myBitfield.length);			
+			}
+			Peer.sendMessage(new Request(receiverID, senderID, pieceDex)); 
+		}
 	}
 	
-	public void writePieceToFile(){
-		String filename = "";
-	//	FileOutputStream out = new FileOutputStream(); 
+	public void writePieceToFile(byte[] piece){
+		String fileLocation = "/src/peer_"+senderID+"/"+pieceIndex;
+		FileOutputStream out; 
+		try{
+			out = new FileOutputStream(new File(fileLocation)); 
+			out.write(piece);
+			out.close();
+		}
+		catch (FileNotFoundException e)
+		{
+			throw new RuntimeException("Cannot find file: " + fileLocation, e);
+		} catch (IOException e) {
+			throw new RuntimeException("Cannot write to file: " + fileLocation, e);
+		}
+		
 	}
 	
 	
@@ -62,7 +103,8 @@ public class Piece extends Message{
 			pieceBuffer.append(scan.next()); 
 		}
 		piece = pieceBuffer.toString().getBytes();
-				
+		
+		scan.close(); 		
 	}
 	
 	public int getPieceIndex(){
